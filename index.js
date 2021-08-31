4e747973f03d0667737d5687201b8abedce9cbc7
FlorRoselli <82415191+FlorRoselli@users.noreply.github.com>
2021-08-30 22:41:46 -0300

    Add files via upload
    Ejercicios corregidos

--- index.js
+++ index.js
@@ -10,38 +10,36 @@ const helloPath = path.resolve("hello.txt");
 const readFileUsers = async () => {
     //Imprimir en consola el arreglo de usuarios
         try {
-            let data = await fs.readFile(usersPath, "utf8");
-            return data;
+             const data = await fs.readFile(usersPath, "utf8");
+        console.log(data);
         } catch (error) {
-            console.log(error.message)
+            console.log(error)
         };
 
 };
 
-const writeHelloWorld = () => {
+const writeHelloWorld = async() => {
     //Escribir hello world! en el archivo hello.txt
-    fs.writeFile(helloPath, "Hello world!", (error) => {
-        if(error){
-            console.log("No se ha podido escribir en este archivo")
-        }
-    });
+    try {
+        await fs.writeFile(helloPath, "hello world!");
+    } catch (error) {
+        console.log(error)   
+    }
     
 };
 
-const addUser = (username) => {
+const addUser = async(username) => {
     //Agregar un usuario en la lista users.json
-    fs.readFile(usersPath, "utf8", function readFileCallback(err, data){
-        if (err){
-            console.log(err);
-        }else{
-            let obj = JSON.parse(data);
-            obj.Push(username);
-            let json = JSON.stringify(obj);
-            fs.writeFile(usersPath, json, "utf8", callback);
-        }
-    });
-}
+    try {
+        const data = await fs.readFile(usersPath, "utf8");
+        const users = JSON.parse(data);
+        users.push(username);
 
+        await fs.writeFile(usersPath, JSON.stringify(users));
+    } catch (error) {
+       console.log(error)
+   };
+};
 //No hace falta ejecutar las funciones
 
 module.exports = {
